c5d71e9182beea33e135f219df9f0bd5ce30e770
unknown <aguit.inan@gmail.com>
2024-03-02 17:26:29 +0100

    feat: add DependencyInjectionExtensions.cs

---
+++ ProductAPI/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Datas;
+
+namespace ProductAPI.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void InjectDependancies(this WebApplicationBuilder builder)
+        {
+            builder.AddDatabase();
+        }
+
+        private static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
