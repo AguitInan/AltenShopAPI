efff0b5e48fe1f728acf9f989394b2e1038348f6
unknown <aguit.inan@gmail.com>
2024-03-02 17:27:50 +0100

    feat: update Program.cs

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,6 +1,22 @@
+using ProductAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Adding CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.InjectDependancies();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
