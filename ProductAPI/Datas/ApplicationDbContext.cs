4ba05b1b57c9a17420965da7612004fdf839139f
unknown <aguit.inan@gmail.com>
2024-03-02 17:24:29 +0100

    feat: add ApplicationDbContext.cs

---
+++ ProductAPI/Datas/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCore.Models;
+using ProductCore.Datas;
+
+namespace ProductAPI.Datas
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(InitialProduct.products);
+        }
+    }
+}
