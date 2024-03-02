c03044a3c05af3130cd19d88c3a3ef72549b8106
unknown <aguit.inan@gmail.com>
2024-03-02 17:18:16 +0100

    feat: add Product.cs

---
+++ ProductCore/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductCore.Models
+{
+    [Table("product")]
+    public class Product
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("code")]
+        public string Code { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
+        [Column("quantity")]
+        public int Quantity { get; set; }
+        [Column("inventory_status")]
+        public string InventoryStatus { get; set; }
+        [Column("category")]
+        public string Category { get; set; }
+        [Column("image")]
+        public string? Image { get; set; }
+        [Column("rating")]
+        public int? Rating { get; set; }
+    }
+}
