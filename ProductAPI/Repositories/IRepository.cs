97f2ca0cddd741d7871a5ec4df5a1f0e238dac4d
unknown <aguit.inan@gmail.com>
2024-03-05 16:22:27 +0100

    feat: update IRepository.cs

--- ProductAPI/Repositories/IRepository.cs
+++ ProductAPI/Repositories/IRepository.cs
@@ -5,14 +5,14 @@ namespace ProductAPI.Repositories
     public interface IRepository<TEntity>
     {
         //CREATE
-        Task<int> Add(TEntity contact);
+        Task<int> Add(TEntity product);
         // READ
         Task<TEntity?> GetById(int id);
         Task<TEntity?> Get(Expression<Func<TEntity, bool>> predicate);
         Task<List<TEntity>> GetAll();
         Task<List<TEntity>> GetAll(Expression<Func<TEntity, bool>> predicate);
         // UPDATE
-        Task<bool> Update(TEntity contact);
+        Task<bool> Update(TEntity product);
         // DELETE
         Task<bool> Delete(int id);
     }
